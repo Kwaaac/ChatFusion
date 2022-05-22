9975ecdcd6cf510b75c4fad86c917d322ecf39ae
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2022-05-22 14:35:44 +0200

    server Init start

--- src/test/main/java/reader/SocketAdressReaderTest.java
+++ src/test/main/java/reader/SocketAdressReaderTest.java
@@ -1,6 +1,6 @@
 package test.main.java.reader;
 
-import main.java.reader.InetSocketAdressReader;
+import main.java.reader.InetSocketAddressReader;
 import main.java.reader.Reader;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +14,7 @@ class SocketAdressReaderTest {
     @Test
     public void ipv4() {
         var expected = new InetSocketAddress("255.255.255.255", 8080);
+        System.out.println(expected.getHostString());
         var bb = ByteBuffer.allocate(1024);
         bb.putInt(4);
         bb.put((byte) 255);
@@ -21,7 +22,7 @@ class SocketAdressReaderTest {
         bb.put((byte) 255);
         bb.put((byte) 255);
         bb.putInt(8080);
-        var sr = new InetSocketAdressReader();
+        var sr = new InetSocketAddressReader();
         assertEquals(Reader.ProcessStatus.DONE, sr.process(bb, 16));
         assertEquals(expected, sr.get());
     }
